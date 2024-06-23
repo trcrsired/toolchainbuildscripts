a248e7b376bd3ff9281d706f3d1eb11520f63d84
cqwrteur <100043421+trcrsired@users.noreply.github.com>
2024-06-23 05:01:51 -0400

    llvm native fix clang

--- llvm/native.sh
+++ llvm/native.sh
@@ -51,7 +51,7 @@ git pull --quiet
 if [ ! -d "${currentpath}/llvm" ]; then
 mkdir -p ${currentpath}/llvm
 cd ${currentpath}/llvm
-cmake -GNinja $LLVMPROJECTPATH/llvm -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_C_COMPILER=clang++ -DLLVM_ENABLE_LLD=On -DLLVM_ENABLE_LTO=thin -DCMAKE_INSTALL_PREFIX=${LLVMINSTALLPATH} -DBUILD_SHARED_LIBS=On -DLLVM_ENABLE_PROJCTS="clang;clang-tools-extra;lld;lldb" -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind;compiler-rt"
+cmake -GNinja $LLVMPROJECTPATH/llvm -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_ASM_COMPILER=clang -DLLVM_ENABLE_LLD=On -DLLVM_ENABLE_LTO=thin -DCMAKE_INSTALL_PREFIX=${LLVMINSTALLPATH} -DBUILD_SHARED_LIBS=On -DLLVM_ENABLE_PROJCTS="clang;clang-tools-extra;lld;lldb" -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind;compiler-rt"
 fi
 
 ninja install/strip
