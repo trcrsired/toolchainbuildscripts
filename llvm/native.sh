0323333f46ba40734881aa1a78ab44378312f002
cqwrteur <100043421+trcrsired@users.noreply.github.com>
2024-06-23 04:59:55 -0400

    llvm native

---
+++ llvm/native.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+TARGETTRIPLE=$(clang -print-target-triple)
+currentpath=$(realpath .)/.llvmartifacts/${TARGETTRIPLE}
+if [ ! -d ${currentpath} ]; then
+	mkdir -p ${currentpath}
+	cd ${currentpath}
+fi
+
+if [ -z ${TOOLCHAINS_BUILD+x} ]; then
+	TOOLCHAINS_BUILD=$currentpath/toolchains_build
+fi
+
+if [ -z ${TOOLCHAINSPATH+x} ]; then
+	TOOLCHAINSPATH=$currentpath/toolchains
+fi
+
+mkdir -p $TOOLCHAINSPATH
+TOOLCHAINS_LLVMPATH=$TOOLCHAINSPATH/llvm
+mkdir -p $TOOLCHAINS_LLVMPATH
+TOOLCHAINS_LLVMSYSROOTSPATH=${TOOLCHAINS_LLVMPATH}/${TARGETTRIPLE}
+mkdir -p $TOOLCHAINS_LLVMSYSROOTSPATH
+
+mkdir -p $TOOLCHAINS_BUILD
+mkdir -p $TOOLCHAINSPATH
+
+clang_path=`which clang`
+clang_directory=$(dirname "$clang_path")
+clang_version=$(clang --version | grep -oP '\d+\.\d+\.\d+')
+clang_major_version="${clang_version%%.*}"
+llvm_install_directory="$clang_directory/.."
+clangbuiltin="$llvm_install_directory/lib/clang/$clang_major_version"
+LLVMINSTALLPATH=${TOOLCHAINS_LLVMSYSROOTSPATH}/llvm
+
+if [[ ${NEEDSUDOCOMMAND} == "yes" ]]; then
+sudocommand="sudo "
+else
+sudocommand=
+fi
+
+if [ -z ${LLVMPROJECTPATH+x} ]; then
+LLVMPROJECTPATH=$TOOLCHAINS_BUILD/llvm-project
+fi
+
+if [ ! -d "$LLVMPROJECTPATH" ]; then
+git clone git@github.com:llvm/llvm-project.git $LLVMPROJECTPATH
+fi
+cd "$LLVMPROJECTPATH"
+git pull --quiet
+
+if [ ! -d "${currentpath}/llvm" ]; then
+mkdir -p ${currentpath}/llvm
+cd ${currentpath}/llvm
+cmake -GNinja $LLVMPROJECTPATH/llvm -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_C_COMPILER=clang++ -DLLVM_ENABLE_LLD=On -DLLVM_ENABLE_LTO=thin -DCMAKE_INSTALL_PREFIX=${LLVMINSTALLPATH} -DBUILD_SHARED_LIBS=On -DLLVM_ENABLE_PROJCTS="clang;clang-tools-extra;lld;lldb" -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind;compiler-rt"
+fi
+
+ninja install/strip
+
+if [ ! -f ${TOOLCHAINS_LLVMSYSROOTSPATH}.tar.xz ]; then
+	cd $TOOLCHAINS_LLVMPATH
+	XZ_OPT=-e9T0 tar cJf ${TARGETTRIPLE}.tar.xz ${TARGETTRIPLE}
+	chmod 755 ${TARGETTRIPLE}.tar.xz
+fi
