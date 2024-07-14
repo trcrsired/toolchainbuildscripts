4268c1532dd106a0987a746d2b79b5a80bc087f0
trcrsired <uwgghhbcad@gmail.com>
2024-07-13 21:53:20 -0400

    clone

--- gccbuild/x86_64-w64-mingw32/clonegccbinutils.sh
+++ gccbuild/x86_64-w64-mingw32/clonegccbinutils.sh
@@ -37,3 +37,28 @@ ln -s ../gcc/mpc mpc
 ln -s ../gcc/gettext gettext
 ln -s ../gcc/isl isl
 fi
+
+if [ -z ${CLONELINUX+x} ]; then
+if [ ! -d "$TOOLCHAINS_BUILD/linux" ]; then
+cd "$TOOLCHAINS_BUILD"
+git clone https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+if [ $? -ne 0 ]; then
+echo "linux clone failed"
+exit 1
+fi
+fi
+cd "$TOOLCHAINS_BUILD/linux"
+git pull --quiet
+fi
+
+if [ -z ${CLONEMINGW64+x} ]; then
+cd "$TOOLCHAINS_BUILD"
+if [ ! -d "$TOOLCHAINS_BUILD/mingw-w64" ]; then
+git clone https://git.code.sf.net/p/mingw-w64/mingw-w64
+if [ $? -ne 0 ]; then
+echo "mingw-w64 clone failed"
+fi
+fi
+cd "$TOOLCHAINS_BUILD/mingw-w64"
+git pull --quiet
+fi
