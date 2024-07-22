fbc10919621a2806d1bba79383e70ce6adfc1b39
cqwrteur <100043421+trcrsired@users.noreply.github.com>
2024-07-22 11:46:09 -0400

    wine

--- wine/wine.sh
+++ wine/wine.sh
@@ -372,6 +372,62 @@ echo "$(date --iso-8601=seconds)" > $currentpath/freetype/.installsuccess
 fi
 
 
+mkdir -p "$currentpath"
+git pull --quiet
+if [ ! -d "$TOOLCHAINS_BUILD/xorgproto" ]; then
+cd "$TOOLCHAINS_BUILD"
+git clone git@gitlab.freedesktop.org:xorg/proto/xorgproto.git
+if [ $? -ne 0 ]; then
+echo "x11 clone failed"
+exit 1
+fi
+fi
+
+if [ ! -f $TOOLCHAINS_BUILD/xorgproto/.autogensuccess ]; then
+mkdir -p $TOOLCHAINS_BUILD/xorgproto
+cd $TOOLCHAINS_BUILD/xorgproto
+NOCONFIGURE=1 ./autogen.sh
+if [ $? -ne 0 ]; then
+echo "xorgproto autogen failed"
+exit 1
+fi
+echo "$(date --iso-8601=seconds)" > ${TOOLCHAINS_BUILD}/xorgproto/.autogensuccess
+fi
+
+
+if [ ! -f $currentpath/xorgproto/.configuresuccess ]; then
+mkdir -p $currentpath/xorgproto
+cd $currentpath/xorgproto
+STRIP=llvm-strip ${TOOLCHAINS_BUILD}/xorgproto/configure --disable-nls --disable-werror --host=$HOST --prefix=$currentpath/installs --cache-file=$currentpath/xorgproto/config.cache --enable-malloc0returnsnull 
+if [ $? -ne 0 ]; then
+echo "xorgproto configuresuccess failed"
+exit 1
+fi
+echo "$(date --iso-8601=seconds)" > ${TOOLCHAINS_BUILD}/xorgproto/.configuresuccess
+fi
+
+if [ ! -f $currentpath/xorgproto/.buildsuccess ]; then
+mkdir -p $currentpath/xorgproto
+cd ${currentpath}/xorgproto
+make -j$(nproc)
+if [ $? -ne 0 ]; then
+echo "xorgproto build failure"
+exit 1
+fi
+echo "$(date --iso-8601=seconds)" > $currentpath/xorgproto/.buildsuccess
+fi
+
+if [ ! -f $currentpath/xorgproto/.installsuccess ]; then
+cd ${currentpath}/xorgproto
+make install -j$(nproc)
+if [ $? -ne 0 ]; then
+echo "xorgproto install failure"
+exit 1
+fi
+echo "$(date --iso-8601=seconds)" > $currentpath/xorgproto/.installsuccess
+fi
+
+cp -r --preserve=links $currentpath/installs/* $SYSROOT/
 
 mkdir -p "$currentpath"
 git pull --quiet
