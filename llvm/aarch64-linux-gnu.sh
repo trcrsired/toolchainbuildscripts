237f88c46375df326aee52d45db61d16b754e551
cqwrteur <100043421+trcrsired@users.noreply.github.com>
2024-07-21 20:07:40 -0400

    aarch64

---
+++ llvm/aarch64-linux-gnu.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+if [ -z ${ARCH+x} ]; then
+ARCH=aarch64
+fi
+TARGETTRIPLE_CPU=${ARCH}
+TARGETTRIPLE=${TARGETTRIPLE_CPU}-linux-gnu
+currentpath=$(realpath .)/.llvmartifacts/${TARGETTRIPLE}
+if [ ! -d ${currentpath} ]; then
+	mkdir -p ${currentpath}
+	cd ${currentpath}
+fi
+if [ -z ${TOOLCHAINS_BUILD+x} ]; then
+	TOOLCHAINS_BUILD=$HOME/toolchains_build
+fi
+
+if [ -z ${TOOLCHAINSPATH+x} ]; then
+	TOOLCHAINSPATH=$HOME/toolchains
+fi
+mkdir -p $TOOLCHAINSPATH
+TOOLCHAINS_LLVMPATH=$TOOLCHAINSPATH/llvm
+mkdir -p $TOOLCHAINS_LLVMPATH
+
+TOOLCHAINS_LLVMSYSROOTSPATH="$TOOLCHAINS_LLVMPATH/${TARGETTRIPLE}"
+
+mkdir -p $TOOLCHAINS_LLVMSYSROOTSPATH
+
+mkdir -p $TOOLCHAINS_BUILD
+mkdir -p $TOOLCHAINSPATH
+
+clang_path=`which clang`
+clang_directory=$(dirname "$clang_path")
+clang_version=$(clang --version | grep -oP '\d+\.\d+\.\d+')
+clang_major_version="${clang_version%%.*}"
+llvm_install_directory="$clang_directory/.."
+clangbuiltin="$llvm_install_directory/lib/clang/$clang_major_version"
+LLVMINSTALLPATH=${TOOLCHAINS_LLVMSYSROOTSPATH}/llvm
+
+if [[ $1 == "restart" ]]; then
+	echo "restarting"
+	rm -rf "${currentpath}"
+	rm -rf "${TOOLCHAINS_LLVMSYSROOTSPATH}"
+	rm "${TOOLCHAINS_LLVMSYSROOTSPATH}.tar.xz"
+	echo "restart done"
+fi
+
+if [ -z ${LLVMPROJECTPATH+x} ]; then
+LLVMPROJECTPATH=$TOOLCHAINS_BUILD/llvm-project
+fi
+
+if [ ! -d "$LLVMPROJECTPATH" ]; then
+git clone git@github.com:llvm/llvm-project.git $LLVMPROJECTPATH
+fi
+cd "$LLVMPROJECTPATH"
+git pull --quiet
+
+cd "$TOOLCHAINS_BUILD"
+if [ ! -d "$TOOLCHAINS_BUILD/zlib" ]; then
+git clone git@github.com:trcrsired/zlib.git
+fi
+cd "$TOOLCHAINS_BUILD/zlib"
+git pull --quiet
+
+
+if ! command -v "$TARGETTRIPLE-gcc" &> /dev/null
+    echo "$TARGETTRIPLE-gcc not exists"
+    exit 1
+fi
+
+gccpath=$(command -v "$TARGETTRIPLE-gcc")
+gccbinpath=$(dirname "$gccpath")
+SYSROOTPATH=$(dirname "$gccbinpath")
+SYSROOTTRIPLEPATH=$SYSROOTPATH/$TARGETTRIPLE
+
+if [ ! -f "${SYSROOTPATH}/include/zlib.h" ]; then
+mkdir -p "$CURRENTTRIPLEPATH/zlib"
+cd $CURRENTTRIPLEPATH/zlib
+cmake -GNinja ${TOOLCHAINS_BUILD}/zlib -DCMAKE_SYSROOT=$SYSROOTPATH -DCMAKE_RC_COMPILER=llvm-windres \
+	-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_ASM_COMPILER=clang \
+	-DCMAKE_INSTALL_PREFIX=$SYSROOTTRIPLEPATH \
+	-DCMAKE_CROSSCOMPILING=On \
+	-DCMAKE_FIND_ROOT_PATH=${SYSROOTTRIPLEPATH} \
+	-DCMAKE_SYSTEM_PROCESSOR=$TARGETTRIPLE_CPU \
+	-DCMAKE_SYSTEM_NAME=Linux \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_C_COMPILER_TARGET=$TARGETTRIPLE \
+	-DCMAKE_CXX_COMPILER_TARGET=$TARGETTRIPLE \
+	-DCMAKE_ASM_COMPILER_TARGET=$TARGETTRIPLE \
+	-DCMAKE_RC_COMPILER_TARGET=$TARGETTRIPLE \
+	-DCMAKE_INTERPROCEDURAL_OPTIMIZATION=On
+ninja install/strip
+fi
+
+if [ ! -d "$LLVMINSTALLPATH" ]; then
+if [ ! -d "$CURRENTTRIPLEPATH/llvm" ]; then
+mkdir -p "$CURRENTTRIPLEPATH/llvm"
+cd $CURRENTTRIPLEPATH/llvm
+cmake $LLVMPROJECTPATH/llvm \
+	-GNinja -DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_ASM_COMPILER=clang \
+	-DCMAKE_SYSROOT=$SYSROOTPATH -DCMAKE_INSTALL_PREFIX=$LLVMINSTALLPATH \
+	-DCMAKE_CROSSCOMPILING=On \
+	-DLLVM_HOST_TRIPLE=$TARGETTRIPLE \
+	-DLLVM_DEFAULT_TARGET_TRIPLE=$TARGETTRIPLE \
+	-DCMAKE_SYSTEM_PROCESSOR=$TARGETTRIPLE_CPU \
+	-DCMAKE_SYSTEM_NAME=Linux \
+	-DBUILD_SHARED_LIBS=On \
+	-DCMAKE_SYSTEM_PROCESSOR=$TARGETTRIPLE_CPU \
+	-DCMAKE_FIND_ROOT_PATH=${SYSROOTTRIPLEPATH} \
+	-DLLVM_ENABLE_LLD=On \
+	-DLLVM_ENABLE_LTO=thin \
+	-DCMAKE_C_COMPILER_TARGET=${TARGETTRIPLE} \
+	-DCMAKE_CXX_COMPILER_TARGET=${TARGETTRIPLE} \
+	-DCMAKE_ASM_COMPILER_TARGET=${TARGETTRIPLE} \
+	-DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;lld;lldb" \
+	-DLLVM_ENABLE_ZLIB=FORCE_ON \
+	-DZLIB_INCLUDE_DIR=$SYSROOTTRIPLEPATH/include \
+	-DZLIB_LIBRARY=$SYSROOTTRIPLEPATH/lib/libz.a \
+	-DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER \
+	-DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY \
+	-DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY
+fi
+cd $CURRENTTRIPLEPATH/llvm
+ninja
+ninja install/strip
+fi
