82d636534e2d00607a48132271864a58cfe845c8
cqwrteur <100043421+trcrsired@users.noreply.github.com>
2024-07-23 06:08:03 -0400

    generic linux gnu

---
+++ llvm/x86_64-generic-linux-gnu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z ${ARCH+x} ]; then
+	ARCH=x86_64
+fi
+if [ -z ${TARGETTRIPLE+x} ]; then
+	TARGETTRIPLE=x86_64-generic-linux-gnu
+fi
+TARGETTRIPLE=$TARGETTRIPLE ARCH=$ARCH ./aarch64-linux-gnu.sh "$@"
+
+if [ $? -ne 0 ]; then
+exit 1
+fi
