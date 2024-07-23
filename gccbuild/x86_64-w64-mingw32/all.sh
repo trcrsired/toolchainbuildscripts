0ec40142f69d98feaa450eb1c8c1b424795f21a5
cqwrteur <100043421+trcrsired@users.noreply.github.com>
2024-07-22 20:58:44 -0400

    x86_64-linux-gnu

--- gccbuild/x86_64-w64-mingw32/all.sh
+++ gccbuild/x86_64-w64-mingw32/all.sh
@@ -1,7 +1,6 @@
 ./native.sh "$@"
 ./x86_64-w64-mingw32.sh "$@"
-#./x86_64-linux-gnu-crossback.sh "$@"
-./x86_64-generic-linux-gnu.sh "$@"
+./x86_64-linux-gnu.sh "$@"
 ./aarch64-linux-gnu.sh "$@"
 ./x86_64-elf.sh "$@"
 ./loongarch64-linux-gnu.sh "$@"
--- gccbuild/x86_64-w64-mingw32/x86_64-linux-gnu.sh
+++ gccbuild/x86_64-w64-mingw32/x86_64-linux-gnu.sh
@@ -1,40 +1,13 @@
-if [ false ]; then
+#!/bin/bash
 
-mkdir -p ${currentpath}/hostbuild
-mkdir -p ${currentpath}/hostbuild/$HOST
-cd ${currentpath}/hostbuild/$HOST
-mkdir -p ${currentpath}/hostbuild/$HOST/binutils-gdb
-cd ${currentpath}/hostbuild/$HOST/binutils-gdb
-if [ ! -f Makefile ]; then
-$TOOLCHAINS_BUILD/binutils-gdb/configure --disable-nls --disable-werror --enable-gold $CANADIANCROSSTRIPLETTRIPLETS --prefix=$CANADIANHOSTPREFIX
+if [ -z ${HOST+x} ]; then
+	HOST=x86_64-generic-linux-gnu
 fi
-
-if [ ! -d $CANADIANHOSTPREFIX/lib/bfd-plugins ]; then
-make -j$(nproc)
-make install-strip -j$(nproc)
+if [ -z ${ARCH+x} ]; then
+	ARCH=x86_64
 fi
-cd ${currentpath}/hostbuild/$HOST
+HOST=$HOST ARCH=$ARCH ./riscv64-linux-gnu.sh "$@"
 
-mkdir -p ${currentpath}/hostbuild/$HOST/gcc
-cd ${currentpath}/hostbuild/$HOST/gcc
-if [ ! -f Makefile ]; then
-$TOOLCHAINS_BUILD/gcc/configure --with-gxx-libcxx-include-dir=$CANADIANHOSTPREFIX/include/c++/v1 --prefix=$CANADIANHOSTPREFIX $CANADIANCROSSTRIPLETTRIPLETS $GCCCONFIGUREFLAGSCOMMON
-fi
-if [ ! -d $CANADIANHOSTPREFIX/lib/gcc ]; then
-make -j$(nproc)
-make install-strip -j$(nproc)
-fi
-
-if [ ! -f $CANADIANHOSTPREFIX/include/stdio.h ]; then
-	cp --preserve=links -r ${currentpath}/install/linux/* $CANADIANHOSTPREFIX/
-	cp --preserve=links -r ${currentpath}/install/glibc/canadian/include $CANADIANHOSTPREFIX/
-	rm -rf ${currentpath}/install/glibc/libs
-	cp -r ${currentpath}/install/glibc/canadian/lib64 ${currentpath}/install/glibc/libs
-	cp -r ${currentpath}/install/glibc/canadian/libx32 ${currentpath}/install/glibc/libs/
-	mv ${currentpath}/install/glibc/libs/libx32 ${currentpath}/install/glibc/libs/x32
-	cp -r ${currentpath}/install/glibc/canadian/lib32 ${currentpath}/install/glibc/libs/
-	mv ${currentpath}/install/glibc/libs/lib32 ${currentpath}/install/glibc/libs/32
-fi
-
-
+if [ $? -ne 0 ]; then
+exit 1
 fi
