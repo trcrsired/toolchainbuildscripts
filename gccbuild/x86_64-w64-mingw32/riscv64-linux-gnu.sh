d108f6ed505fc4e8980b4bb143567d1fcccc9fb0
trcrsired <uwgghhbcad@gmail.com>
2024-07-28 14:16:57 -0400

    try

--- gccbuild/x86_64-w64-mingw32/riscv64-linux-gnu.sh
+++ gccbuild/x86_64-w64-mingw32/riscv64-linux-gnu.sh
@@ -113,16 +113,100 @@ fi
 cd "$TOOLCHAINS_BUILD/gcc"
 git pull --quiet
 
-if [ ! -L "$TOOLCHAINS_BUILD/gcc/gmp" ]; then
-cd $TOOLCHAINS_BUILD/gcc
-./contrib/download_prerequisites
+
+cd "$TOOLCHAINS_BUILD"
+if [ ! -d "$TOOLCHAINS_BUILD/gmp" ]; then
+hg clone https://gmplib.org/repo/gmp
+if [ $? -ne 0 ]; then
+echo "gmp clone failed"
+exit 1
+fi
+fi
+cd "$TOOLCHAINS_BUILD/gmp"
+hg pull --quiet
+if [ ! -f "$TOOLCHAINS_BUILD/gmp/configure" ]; then
+cd $TOOLCHAINS_BUILD/gmp
+autoreconf -i
+fi
+
+
+cd "$TOOLCHAINS_BUILD"
+if [ ! -d "$TOOLCHAINS_BUILD/mpfr" ]; then
+git clone https://gitlab.inria.fr/mpfr/mpfr.git
+if [ $? -ne 0 ]; then
+echo "mpfr clone failed"
+exit 1
+fi
+fi
+cd "$TOOLCHAINS_BUILD/mpfr"
+git pull --quiet
+if [ ! -f "$TOOLCHAINS_BUILD/mpfr/configure" ]; then
+cd $TOOLCHAINS_BUILD/mpfr
+./autogen.sh
+fi
+
+cd "$TOOLCHAINS_BUILD"
+if [ ! -d "$TOOLCHAINS_BUILD/mpc" ]; then
+git clone https://gitlab.inria.fr/mpc/mpc.git
+if [ $? -ne 0 ]; then
+echo "mpc clone failed"
+exit 1
+fi
+fi
+cd "$TOOLCHAINS_BUILD/mpc"
+git pull --quiet
+if [ ! -f "$TOOLCHAINS_BUILD/mpc/configure" ]; then
+cd $TOOLCHAINS_BUILD/mpc
+autoreconf -i
+fi
+
+
+cd "$TOOLCHAINS_BUILD"
+if [ ! -d "$TOOLCHAINS_BUILD/isl" ]; then
+git clone git://repo.or.cz/isl.git
+if [ $? -ne 0 ]; then
+echo "isl clone failed"
+exit 1
+fi
+fi
+cd "$TOOLCHAINS_BUILD/isl"
+git pull --quiet
+if [ ! -f "$TOOLCHAINS_BUILD/isl/configure" ]; then
+cd $TOOLCHAINS_BUILD/isl
+./autogen.sh
+fi
+
+
+cd "$TOOLCHAINS_BUILD"
+if [ ! -d "$TOOLCHAINS_BUILD/gettext" ]; then
+git clone git://git.savannah.gnu.org/gettext.git
+if [ $? -ne 0 ]; then
+echo "gettext clone failed"
+exit 1
+fi
+fi
+cd "$TOOLCHAINS_BUILD/gettext"
+git pull --quiet
+if [ ! -f "$TOOLCHAINS_BUILD/gettext/configure" ]; then
+cd $TOOLCHAINS_BUILD/gettext
+./autogen.sh
 fi
 
 if [ ! -L "$TOOLCHAINS_BUILD/binutils-gdb/gmp" ]; then
 cd $TOOLCHAINS_BUILD/binutils-gdb
-ln -s ../gcc/gmp gmp
-ln -s ../gcc/mpfr mpfr
-ln -s ../gcc/mpc mpc
+ln -s $TOOLCHAINS_BUILD/gmp gmp
+ln -s $TOOLCHAINS_BUILD/mpfr mpfr
+ln -s $TOOLCHAINS_BUILD/mpc mpc
+ln -s $TOOLCHAINS_BUILD/isl isl
+fi
+
+if [ ! -L "$TOOLCHAINS_BUILD/gcc/gmp" ]; then
+cd $TOOLCHAINS_BUILD/gcc
+ln -s $TOOLCHAINS_BUILD/gmp gmp
+ln -s $TOOLCHAINS_BUILD/mpfr mpfr
+ln -s $TOOLCHAINS_BUILD/mpc mpc
+ln -s $TOOLCHAINS_BUILD/isl isl
+ln -s $TOOLCHAINS_BUILD/gettext gettext
 fi
 
 cd "$TOOLCHAINS_BUILD"
