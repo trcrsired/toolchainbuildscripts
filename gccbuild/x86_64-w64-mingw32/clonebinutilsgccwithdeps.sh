3c7b3249e8c47f70244f57c892fe8dbb3da9b052
trcrsired <uwgghhbcad@gmail.com>
2024-08-27 08:39:25 -0400

    add CLONE_IN_CHINA detection?

--- gccbuild/x86_64-w64-mingw32/clonebinutilsgccwithdeps.sh
+++ gccbuild/x86_64-w64-mingw32/clonebinutilsgccwithdeps.sh
@@ -1,4 +1,19 @@
 cd "$TOOLCHAINS_BUILD"
+
+if [[ ${CLONE_IN_CHINA} == "yes" ]]; then
+if [ ! -d "$TOOLCHAINS_BUILD/binutils-gdb" ]; then
+git clone https://mirrors.tuna.tsinghua.edu.cn/git/binutils-gdb.git
+if [ $? -ne 0 ]; then
+echo "binutils-gdb from tsinghua.edu clone failed"
+exit 1
+fi
+git remote add upstream git://sourceware.org/git/binutils-gdb.git
+fi
+git fetch upstream
+git merge upstream/master
+
+else
+
 if [ ! -d "$TOOLCHAINS_BUILD/binutils-gdb" ]; then
 git clone git://sourceware.org/git/binutils-gdb.git
 if [ $? -ne 0 ]; then
@@ -8,6 +23,7 @@ fi
 fi
 cd "$TOOLCHAINS_BUILD/binutils-gdb"
 git pull --quiet
+fi
 
 cd "$TOOLCHAINS_BUILD"
 if [ ! -d "$TOOLCHAINS_BUILD/gcc" ]; then
@@ -38,11 +54,34 @@ fi
 
 
 cd "$TOOLCHAINS_BUILD"
+
+if [[ ${CLONE_IN_CHINA} == "yes" ]]; then
+
+if [ ! -d "$TOOLCHAINS_BUILD/mpfr" ]; then
+git clone https://gitee.com/hic_0757/mpfr.git
+if [ $? -ne 0 ]; then
+echo "mpfc from gitee.com clone failed"
+exit 1
+fi
+git remote add upstream https://gitlab.inria.fr/mpfr/mpfr.git
+fi
+git fetch upstream
+if [ $? -ne 0 ]; then
+echo "mpfr fetch from upstream failed"
+exit 1
+fi
+git merge upstream/master
+if [ $? -ne 0 ]; then
+echo "mpfr merge from upstream/main failed"
+exit
+fi
+else
 if [ ! -d "$TOOLCHAINS_BUILD/mpfr" ]; then
 git clone https://gitlab.inria.fr/mpfr/mpfr.git
 if [ $? -ne 0 ]; then
 echo "mpfr clone failed"
 exit 1
+fi
 fi
 fi
 cd "$TOOLCHAINS_BUILD/mpfr"
@@ -52,6 +91,27 @@ cd $TOOLCHAINS_BUILD/mpfr
 ./autogen.sh
 fi
 
+if [[ ${CLONE_IN_CHINA} == "yes" ]]; then
+
+if [ ! -d "$TOOLCHAINS_BUILD/mpc" ]; then
+git clone https://gitee.com/mirrors_gitlab_inria_fr/mpc.git
+if [ $? -ne 0 ]; then
+echo "mpc from gitee.com clone failed"
+exit 1
+fi
+git remote add upstream https://gitlab.inria.fr/mpc/mpc.git
+fi
+git fetch upstream
+if [ $? -ne 0 ]; then
+echo "mpc fetch from upstream failed"
+exit 1
+fi
+git merge upstream/master
+if [ $? -ne 0 ]; then
+echo "mpc merge from upstream/main failed"
+exit
+fi
+else
 cd "$TOOLCHAINS_BUILD"
 if [ ! -d "$TOOLCHAINS_BUILD/mpc" ]; then
 git clone https://gitlab.inria.fr/mpc/mpc.git
@@ -60,6 +120,8 @@ echo "mpc clone failed"
 exit 1
 fi
 fi
+fi
+
 cd "$TOOLCHAINS_BUILD/mpc"
 git pull --quiet
 if [ ! -f "$TOOLCHAINS_BUILD/mpc/configure" ]; then
@@ -69,6 +131,28 @@ fi
 
 
 cd "$TOOLCHAINS_BUILD"
+
+if [[ ${CLONE_IN_CHINA} == "yes" ]]; then
+
+if [ ! -d "$TOOLCHAINS_BUILD/isl" ]; then
+git clone https://gitee.com/mirrors_community_repo_or/isl_1
+if [ $? -ne 0 ]; then
+echo "mpfc from gitee.com clone failed"
+exit 1
+fi
+git remote add upstream git://repo.or.cz/isl.git
+fi
+git fetch upstream
+if [ $? -ne 0 ]; then
+echo "isl fetch from upstream failed"
+exit 1
+fi
+git merge upstream/master
+if [ $? -ne 0 ]; then
+echo "isl merge from upstream/main failed"
+exit
+fi
+else
 if [ ! -d "$TOOLCHAINS_BUILD/isl" ]; then
 git clone git://repo.or.cz/isl.git
 if [ $? -ne 0 ]; then
@@ -78,6 +162,7 @@ fi
 fi
 cd "$TOOLCHAINS_BUILD/isl"
 git pull --quiet
+fi
 if [ ! -f "$TOOLCHAINS_BUILD/isl/configure" ]; then
 cd $TOOLCHAINS_BUILD/isl
 ./autogen.sh
