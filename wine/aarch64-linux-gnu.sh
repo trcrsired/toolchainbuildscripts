1fc6650499af1b5590286e08e9cb2f9e267eb951
trcrsired <uwgghhbcad@gmail.com>
2024-08-28 23:41:34 -0400

    aarch64

--- wine/aarch64-linux-gnu.sh
+++ wine/aarch64-linux-gnu.sh
@@ -1,1 +1,1 @@
-CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ CC_FOR_HOST=aarch64-linux-gnu-gcc CXX_FOR_HOST=aarch64-linux-gnu-g++ ARCH=aarch64 ./wine.sh "$@"
+CC_FOR_HOST=aarch64-linux-gnu-gcc CXX_FOR_HOST=aarch64-linux-gnu-g++ ARCH=aarch64 ./wine.sh "$@"
