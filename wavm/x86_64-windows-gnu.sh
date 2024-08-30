c67724f70af50a710fa6825f915c0234eb4c989a
trcrsired <uwgghhbcad@gmail.com>
2024-08-29 21:29:53 -0400

    fix windows

--- wavm/x86_64-windows-gnu.sh
+++ wavm/x86_64-windows-gnu.sh
@@ -6,5 +6,5 @@ if [ -z ${SYSTEMNAME+x} ]; then
 SYSTEMNAME=Windows
 fi
 
-HOST=$HOST SYSTEMNAME=$SYSTEMNAME ./wavm.sh "$@"
+HOST=$HOST SYSTEMNAME=$SYSTEMNAME EXTRACLAGS="-rtlib=compiler-rt --unwindlib=libunwind" EXTRACXXFLAGS="-rtlib=compiler-rt --unwindlib=libunwind -stdlib=libc++ -lunwind -lc++abi" ./wavm.sh "$@"
 exit $?
