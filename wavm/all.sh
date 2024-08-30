62d57290475ee804a5d458b93388cc41e02f53bd
trcrsired <uwgghhbcad@gmail.com>
2024-08-29 21:31:06 -0400

    android

--- wavm/all.sh
+++ wavm/all.sh
@@ -1,5 +1,22 @@
 ./wavm.sh "$@"
+if [ $? -ne 0 ]; then
+echo "WAVM native failed"
+exit 1
+fi
 ./x86_64-windows-gnu.sh "$@"
+if [ $? -ne 0 ]; then
+echo "WAVM x86_64-windows-gnu failed"
+exit 1
+fi
 HOST=aarch64-windows-gnu ./x86_64-windows-gnu.sh "$@"
+if [ $? -ne 0 ]; then
+echo "WAVM aarch64-windows-gnu failed"
+fi
 ./aarch64-linux-gnu.sh "$@"
+if [ $? -ne 0 ]; then
+echo "WAVM aarch64-linux-gnu failed"
+fi
 ./aarch64-linux-android30.sh "$@"
+if [ $? -ne 0 ]; then
+echo "WAVM aarch64-linux-andoid30 failed"
+fi
