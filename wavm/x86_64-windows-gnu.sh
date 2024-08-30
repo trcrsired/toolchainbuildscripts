fb1c420ea7bf44ca85709bf33b180df433642dce
trcrsired <uwgghhbcad@gmail.com>
2024-08-29 22:24:41 -0400

    windows

--- wavm/x86_64-windows-gnu.sh
+++ wavm/x86_64-windows-gnu.sh
@@ -6,5 +6,5 @@ if [ -z ${SYSTEMNAME+x} ]; then
 SYSTEMNAME=Windows
 fi
 
-HOST=$HOST SYSTEMNAME=$SYSTEMNAME EXTRACFLAGS="-rtlib=compiler-rt --unwindlib=libunwind" EXTRACXXFLAGS="-rtlib=compiler-rt --unwindlib=libunwind -stdlib=libc++ -lunwind -lc++abi" ./wavm.sh "$@"
+HOST=$HOST SYSTEMNAME=$SYSTEMNAME EXTRAFLAGS="-DCMAKE_C_LINKER_DEPFILE_SUPPORTED=Off -DCMAKE_CXX_LINKER_DEPFILE_SUPPORTED=Off -DCMAKE_ASM_LINKER_DEPFILE_SUPPORTED=Off" EXTRACFLAGS="-rtlib=compiler-rt --unwindlib=libunwind" EXTRACXXFLAGS="-rtlib=compiler-rt --unwindlib=libunwind -stdlib=libc++ -lunwind -lc++abi" ./wavm.sh "$@"
 exit $?
