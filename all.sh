46cb196deec3f0ad16d5be145a2efd975c0bd7db
cqwrteur <100043421+trcrsired@users.noreply.github.com>
2024-09-05 20:43:32 -0400

    all.sh

---
+++ all.sh
@@ -0,0 +1,11 @@
+﻿buildallcurrentpath=$(realpath .)
+
+cd $buildallcurrentpath/llvm
+./all.sh "$@"
+cd $buildallcurrentpath/llvm
+./wasm-sysroots.sh "$@"
+cd $buildallcurrentpath/gccbuild/x86_64-w64-mingw32
+./all.sh "$@"
+cd $buildallcurrentpath/wavm
+./all.sh "$@"
+
