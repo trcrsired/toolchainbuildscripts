56aa57f67e2e48f0c76587659a9e30ee3d66500a
cqwrteur <100043421+trcrsired@users.noreply.github.com>
2024-09-05 20:50:05 -0400

    silent

---
+++ all-silent.sh
@@ -0,0 +1,7 @@
+﻿if [[ $1 == "restart" ]]; then
+	echo "restarting"
+	rm -f "$(realpath .)/buildstderrlogs.txt"
+	echo "restart done"
+fi
+
+./all.sh "$@" > /dev/null 2 > "$(realpath .)/buildstderrlogs.txt"
