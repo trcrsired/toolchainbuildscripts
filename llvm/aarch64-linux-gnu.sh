4ba518b68a42f28aff9ed39be7492e059b08b38b
trcrsired <uwgghhbcad@gmail.com>
2024-09-07 04:11:30 -0400

    fix environmental variable

--- llvm/aarch64-linux-gnu.sh
+++ llvm/aarch64-linux-gnu.sh
@@ -81,8 +81,6 @@ then
 fi
 
 gccnativetriplet=$(gcc -dumpmachine)
-export PATH=$LLVMINSTALLPATH/bin:$PATH
-export LD_LIBRARY_PATH=$LLVMINSTALLPATH/lib:$LD_LIBRARY_PATH
 
 gccpath=$(command -v "$TARGETTRIPLE-gcc")
 gccbinpath=$(dirname "$gccpath")
