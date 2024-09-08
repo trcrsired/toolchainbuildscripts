a0e621c21a86b5c2b13dfb27862bc274944bf3f2
trcrsired <uwgghhbcad@gmail.com>
2024-09-08 15:11:14 -0400

    add more build by default

--- gccbuild/x86_64-w64-mingw32/all.sh
+++ gccbuild/x86_64-w64-mingw32/all.sh
@@ -16,3 +16,15 @@ fi
 if ! ./loongarch64-linux-gnu.sh "$@"; then
 exit 1
 fi
+if ! ./loongarch64-linux-musl.sh "$@"; then
+exit 1
+fi
+if ! ./x86_64-freebsd14.sh "$@"; then
+exit 1
+fi
+if ! ./i586-msdosdjgpp.sh "$@"; then
+exit 1
+fi
+if ! ./i686-w64-mingw32.sh "$@"; then
+exit 1
+fi
