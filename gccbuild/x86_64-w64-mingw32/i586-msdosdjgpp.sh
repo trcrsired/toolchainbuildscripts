4ee85e3f480262d2fe37693a40347a30a4eb0270
trcrsired <uwgghhbcad@gmail.com>
2024-09-08 14:57:26 -0400

    djgpp

--- gccbuild/x86_64-w64-mingw32/i586-msdosdjgpp.sh
+++ gccbuild/x86_64-w64-mingw32/i586-msdosdjgpp.sh
@@ -147,6 +147,12 @@ cp -r ${currentpath}/build/${DJCRX}/include $CANADIANPREFIXTARGET/
 cp -r ${currentpath}/build/${DJCRX}/lib $CANADIANPREFIXTARGET/
 fi
 
+cd $BUILD/..
+if [ ! -f $TARGET.tar.xz ]; then
+	XZ_OPT=-e9T0 tar cJf $TARGET.tar.xz $TARGET
+	chmod 755 $TARGET.tar.xz
+fi
+
 cd $CANADIANPREFIX/..
 if [ ! -f $TARGET.tar.xz ]; then
         XZ_OPT=-e9T0 tar cJf $TARGET.tar.xz $TARGET
