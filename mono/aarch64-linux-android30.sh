cde03fb1a47fa8a58e5407dd6aeb00ab5410d2bd
trcrsired <uwgghhbcad@gmail.com>
2024-09-11 00:43:52 -0400

    remove gnu

--- mono/aarch64-linux-android30.sh
+++ mono/aarch64-linux-android30.sh
@@ -2,4 +2,4 @@ if [ -z ${TOOLCHAINSPATH+x} ]; then
 	TOOLCHAINSPATH=$HOME/toolchains
 fi
 
-CC="clang --target=aarch64-linux-gnu-android30 --sysroot=$TOOLCHAINSPATH/llvm/aarch64-linux-android30/aarch64-linux-android30" CXX="clang++ --target=aarch64-linux-gnu-android30 --sysroot=$TOOLCHAINSPATH/llvm/aarch64-linux-android30/aarch64-linux-android30 -I$TOOLCHAINSPATH/llvm/aarch64-linux-android30/aarch64-linux-android30/include/c++/v1" HOST=aarch64-linux-android30 ./mono.sh "$@"
+CC="clang --target=aarch64-linux-android30 --sysroot=$TOOLCHAINSPATH/llvm/aarch64-linux-android30/aarch64-linux-android30" CXX="clang++ --target=aarch64-linux-android30 --sysroot=$TOOLCHAINSPATH/llvm/aarch64-linux-android30/aarch64-linux-android30 -I$TOOLCHAINSPATH/llvm/aarch64-linux-android30/aarch64-linux-android30/include/c++/v1" HOST=aarch64-linux-android30 ./mono.sh "$@"
