68adf7e3929f7c96d7529c84536995521c4a39c8
trcrsired <uwgghhbcad@gmail.com>
2024-09-11 00:43:17 -0400

    fix android

--- wine/aarch64-linux-android30.sh
+++ wine/aarch64-linux-android30.sh
@@ -1,1 +1,4 @@
+if [ -z ${TOOLCHAINSPATH+x} ]; then
+	TOOLCHAINSPATH=$HOME/toolchains
+fi
 enable_wineandroid_drv=no CC=clang CXX=clang++ HOST=aarch64-linux-android30 MINIMUMBUILD=yes SYSROOT=$TOOLCHAINSPATH/llvm/aarch64-linux-android30/aarch64-linux-android30 ARCH=aarch64 CMAKEEXTRAFLAGS="-DCMAKE_CXX_FLAGS=\"-I$TOOLCHAINSPATH/llvm/aarch64-linux-android30/aarch64-linux-android30/include/c++/v1\"" ./wine.sh "$@"
