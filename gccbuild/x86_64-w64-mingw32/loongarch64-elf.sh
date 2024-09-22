095d449a6e6aa1b2a640c5d6542a2cc575d184d7
trcrsired <uwgghhbcad@gmail.com>
2024-09-22 14:40:15 -0400

    loongarch64-elf

--- gccbuild/x86_64-w64-mingw32/loongarch64-elf.sh
+++ gccbuild/x86_64-w64-mingw32/loongarch64-elf.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ -z ${HOST+x} ]; then
-	HOST=loongarch64-elf
+if [ -z ${TARGET+x} ]; then
+	TARGET=loongarch64-elf
 fi
 if [ -z ${ARCH+x} ]; then
 	ARCH=loongarch
 fi
-HOST=$HOST ARCH=$ARCH ./x86_64-elf.sh "$@"
+TARGET=$HOST ARCH=$ARCH ./x86_64-elf.sh "$@"
 
 if [ $? -ne 0 ]; then
 exit 1
