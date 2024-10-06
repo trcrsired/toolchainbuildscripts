b3636c742df7782837617c63e3128e75275b1f01
trcrsired <uwgghhbcad@gmail.com>
2024-10-06 17:33:21 -0400

    fix aarch64-windows-gnu.sh

--- wavm/aarch64-windows-gnu.sh
+++ wavm/aarch64-windows-gnu.sh
@@ -1,2 +1,10 @@
-HOST=aarch64-windows-gnu.sh ./x86_64-windows-gnu.sh "$@"
+if [ -z ${HOST+x} ]; then
+HOST=aarch64-windows-gnu
+fi
+
+if [ -z ${SYSTEMNAME+x} ]; then
+SYSTEMNAME=Windows
+fi
+
+HOST=$HOST SYSTEMNAME=$SYSTEMNAME ./x86_64-windows-gnu.sh "$@"
 exit $?
