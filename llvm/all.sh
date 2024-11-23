d5e725d6363da8c84678c7ae93c6de1a07df88cb
trcrsired <uwgghhbcad@gmail.com>
2024-11-23 00:06:05 -0500

    remove android28 since it does not compile for some unknown reasons

--- llvm/all.sh
+++ llvm/all.sh
@@ -3,7 +3,7 @@
 ./aarch64-windows-gnu.sh "$@"
 ./aarch64-linux-android30.sh "$@"
 ./x86_64-linux-android30.sh "$@"
-./x86_64-linux-android28.sh "$@"
+#./x86_64-linux-android28.sh "$@"
 #ANDROIDAPIVERSION=24 ./aarch64-linux-android30.sh "$@"
 ./aarch64-linux-gnu.sh "$@"
 ./x86_64-generic-linux-gnu.sh "$@"
