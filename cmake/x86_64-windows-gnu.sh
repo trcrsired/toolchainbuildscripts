6b5c6ecb36255d0e3047901ac8c5d22ef2bb66a5
trcrsired <uwgghhbcad@gmail.com>
2024-12-09 22:41:53 -0500

    lld

--- cmake/x86_64-windows-gnu.sh
+++ cmake/x86_64-windows-gnu.sh
@@ -5,4 +5,4 @@ if [ -z ${ARCH+x} ]; then
 	ARCH=x86_64
 fi
 
-CC=clang CXX=clang++ HOST=$ARCH-windows-gnu SYSROOT=$TOOLCHAINSPATH/llvm/$ARCH-windows-gnu/$ARCH-windows-gnu ARCH=$ARCH EXTRACXXFLAGS="-lc++abi -lunwind -rtlib=compiler-rt --unwindlib=libunwind -stdlib=libc++" EXTRACFLAGS="-rtlib=compiler-rt" EXTRAFLAGS="-DCMAKE_USE_OPENSSL=OFF -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_RC_COMPILER=llvm-windres -DCMAKE_RC_FLAGS=\"--target=$ARCH-windows-gnu\" -DCMAKE_RC_COMPILER_TARGET=$ARCH-windows-gnu" ./cmake.sh "$@"
+CC=clang CXX=clang++ HOST=$ARCH-windows-gnu SYSROOT=$TOOLCHAINSPATH/llvm/$ARCH-windows-gnu/$ARCH-windows-gnu ARCH=$ARCH EXTRACXXFLAGS="-lc++abi -lunwind -rtlib=compiler-rt --unwindlib=libunwind -stdlib=libc++ -fuse-ld=lld" EXTRACFLAGS="-rtlib=compiler-rt -fuse-ld=lld" EXTRAFLAGS="-DCMAKE_LINKER=lld -DCMAKE_USE_OPENSSL=OFF -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_RC_COMPILER=llvm-windres -DCMAKE_RC_FLAGS=\"--target=$ARCH-windows-gnu\" -DCMAKE_RC_COMPILER_TARGET=$ARCH-windows-gnu" ./cmake.sh "$@"
--- cmake/x86_64-windows-msvc.sh
+++ cmake/x86_64-windows-msvc.sh
@@ -5,4 +5,4 @@ if [ -z ${ARCH+x} ]; then
 	ARCH=x86_64
 fi
 
-CC=clang CXX=clang++ HOST=$ARCH-windows-msvc SYSROOT=$TOOLCHAINSPATH/windows-msvc-sysroot ARCH=$ARCH EXTRACXXFLAGS="--sysroot=$TOOLCHAINSPATH/windows-msvc-sysroot -D_DLL=1 -lmsvcrt" EXTRACFLAGS="--sysroot=$TOOLCHAINSPATH/windows-msvc-sysroot -D_DLL=1 -lmsvcrt" EXTRAFLAGS="-DCMAKE_USE_OPENSSL=OFF -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_RC_COMPILER=llvm-rc  -DCMAKE_RC_COMPILER_TARGET=$ARCH-windows-msvc -DCMAKE_MT=llvm-mt -DCMAKE_MT_TARGET=$ARCH-windows-msvc" ./cmake.sh "$@"
+CC=clang CXX=clang++ HOST=$ARCH-windows-msvc SYSROOT=$TOOLCHAINSPATH/windows-msvc-sysroot ARCH=$ARCH EXTRACXXFLAGS="--sysroot=$TOOLCHAINSPATH/windows-msvc-sysroot -D_DLL=1 -lmsvcrt -fuse-ld=lld" EXTRACFLAGS="--sysroot=$TOOLCHAINSPATH/windows-msvc-sysroot -D_DLL=1 -lmsvcrt -fuse-ld=lld" EXTRAFLAGS="-DCMAKE_LINKER=lld -DCMAKE_USE_OPENSSL=OFF -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_RC_COMPILER=llvm-rc  -DCMAKE_RC_COMPILER_TARGET=$ARCH-windows-msvc -DCMAKE_MT=llvm-mt -DCMAKE_MT_TARGET=$ARCH-windows-msvc" ./cmake.sh "$@"
