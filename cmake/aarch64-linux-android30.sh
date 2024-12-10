768b6e8a0f2b0b472b90579d8dc25bb92af44418
trcrsired <uwgghhbcad@gmail.com>
2024-12-09 23:56:13 -0500

    shit

--- cmake/aarch64-linux-android30.sh
+++ cmake/aarch64-linux-android30.sh
@@ -5,4 +5,4 @@ if [ -z ${ARCH+x} ]; then
 	ARCH=aarch64
 fi
 
-TOOLCHAINSPATH=$TOOLCHAINSPATH ARCH=$ARCH ./x86_64-linux-gnu.sh "$@"
+TOOLCHAINSPATH=$TOOLCHAINSPATH ARCH=$ARCH ./x86_64-linux-android30.sh "$@"
