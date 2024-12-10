9e442777e66d416d974fa960906c1159fc1a2b5f
trcrsired <uwgghhbcad@gmail.com>
2024-12-09 20:09:41 -0500

    aarch64

--- cmake/aarch64-linux-android30.sh
+++ cmake/aarch64-linux-android30.sh
@@ -1,4 +1,4 @@
 if [ -z ${TOOLCHAINSPATH+x} ]; then
 	TOOLCHAINSPATH=$HOME/toolchains
 fi
-CC=clang CXX=clang++ HOST=aarch64-linux-android30 SYSROOT=$TOOLCHAINSPATH/llvm/aarch64-linux-android30/aarch64-linux-android30 ARCH=aarch64 EXTRACXXFLAGS="-lc++abi -lunwind -DCMAKE_CXX_FLAGS=\"-I$TOOLCHAINSPATH/llvm/aarch64-linux-android30/aarch64-linux-android30/include/c++/v1\"" EXTRAFLAGS="-DCMAKE_USE_OPENSSL=OFF" ./cmake.sh "$@"
+CC=clang CXX=clang++ HOST=aarch64-linux-android30 SYSROOT=$TOOLCHAINSPATH/llvm/aarch64-linux-android30/aarch64-linux-android30 ARCH=aarch64 EXTRACXXFLAGS="-lc++abi -lunwind -DCMAKE_CXX_FLAGS=\"-I$TOOLCHAINSPATH/llvm/aarch64-linux-android30/aarch64-linux-android30/include/c++/v1\" -I$(realpath .)/android" EXTRAFLAGS="-DCMAKE_USE_OPENSSL=OFF" ./cmake.sh "$@"
