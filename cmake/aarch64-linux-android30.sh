dd22353a6b5ae5af5dd92276db9d9570ee64e62d
trcrsired <uwgghhbcad@gmail.com>
2024-12-09 19:50:29 -0500

    fx

--- cmake/aarch64-linux-android30.sh
+++ cmake/aarch64-linux-android30.sh
@@ -1,4 +1,4 @@
 if [ -z ${TOOLCHAINSPATH+x} ]; then
 	TOOLCHAINSPATH=$HOME/toolchains
 fi
-CC=clang CXX=clang++ HOST=aarch64-linux-android30 SYSROOT=$TOOLCHAINSPATH/llvm/aarch64-linux-android30/aarch64-linux-android30 ARCH=aarch64 ./cmake.sh "$@"
+CC=clang CXX=clang++ HOST=aarch64-linux-android30 SYSROOT=$TOOLCHAINSPATH/llvm/aarch64-linux-android30/aarch64-linux-android30 ARCH=aarch64 EXTRACXXFLAGS="-lc++abi -lunwind" ./cmake.sh "$@"
