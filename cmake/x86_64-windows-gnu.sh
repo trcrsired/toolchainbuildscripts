76069a5ad71685d9d8348b2b25f6a215d720fa5a
trcrsired <uwgghhbcad@gmail.com>
2024-12-09 21:30:54 -0500

    wtf??

--- cmake/x86_64-windows-gnu.sh
+++ cmake/x86_64-windows-gnu.sh
@@ -5,4 +5,4 @@ if [ -z ${ARCH+x} ]; then
 	ARCH=x86_64
 fi
 
-CC=clang CXX=clang++ HOST=$ARCH-windows-gnu SYSROOT=$TOOLCHAINSPATH/llvm/$ARCH-windows-gnu/$ARCH-windows-gnu ARCH=$ARCH EXTRACXXFLAGS="-lc++abi -lunwind -rtlib=compiler-rt --unwindlib=libunwind -stdlib=libc++" EXTRACFLAGS="-lc++abi -lunwind -rtlib=compiler-rt --unwindlib=libunwind -stdlib=libc++" EXTRAFLAGS="-DCMAKE_USE_OPENSSL=OFF -DCMAKE_SYSTEM_NAME=Windows" ./cmake.sh "$@"
+CC=clang CXX=clang++ HOST=$ARCH-windows-gnu SYSROOT=$TOOLCHAINSPATH/llvm/$ARCH-windows-gnu/$ARCH-windows-gnu ARCH=$ARCH EXTRACXXFLAGS="-lc++abi -lunwind -rtlib=compiler-rt --unwindlib=libunwind -stdlib=libc++" EXTRACFLAGS="-rtlib=compiler-rt" EXTRAFLAGS="-DCMAKE_USE_OPENSSL=OFF -DCMAKE_SYSTEM_NAME=Windows" ./cmake.sh "$@"
