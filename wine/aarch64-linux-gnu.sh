364e37e86c00faa36152d0a08ccbb8dc6a2f6aa6
trcrsired <uwgghhbcad@gmail.com>
2024-12-15 11:40:43 -0500

    aarch64

--- wine/aarch64-linux-gnu.sh
+++ wine/aarch64-linux-gnu.sh
@@ -1,1 +1,1 @@
-CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ MINIMUMBUILD=yes CC_FOR_HOST=aarch64-linux-gnu-gcc CXX_FOR_HOST=aarch64-linux-gnu-g++ ARCH=aarch64 HOST=aarch64-linux-gnu ./wine.sh "$@"
+ARCH=aarch64 ./linux-gnu.sh
