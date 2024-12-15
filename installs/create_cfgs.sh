4a39a3d8c70fc8ec0d852c59aa67f62f2dea3d3b
trcrsired <uwgghhbcad@gmail.com>
2024-12-15 11:21:04 -0500

    i686 missing

--- installs/create_cfgs.sh
+++ installs/create_cfgs.sh
@@ -71,10 +71,12 @@ create_cfg_file "wasm32-wasip1-noeh.cfg" "wasm32-wasip1" "$ABS_TOOLCHAINSPATH_LL
 # Create msvc .cfg files
 create_cfg_file "x86_64-windows-msvc.cfg" "x86_64-windows-msvc" "$ABS_TOOLCHAINSPATH/windows-msvc-sysroot" "" "-D_DLL=1 -lmsvcrt"
 create_cfg_file "aarch64-windows-msvc.cfg" "aarch64-windows-msvc" "$ABS_TOOLCHAINSPATH/windows-msvc-sysroot" "" "-D_DLL=1 -lmsvcrt"
+create_cfg_file "i686-windows-msvc.cfg" "i686-windows-msvc" "$ABS_TOOLCHAINSPATH/windows-msvc-sysroot" "" "-D_DLL=1 -lmsvcrt"
 
 # Create msvc .cfg files with libcxx
 create_cfg_file "x86_64-windows-msvc-libcxx.cfg" "x86_64-windows-msvc" "$ABS_TOOLCHAINSPATH/windows-msvc-sysroot" "" "-D_DLL=1 -lmsvcrt -stdlib=libc++"
 create_cfg_file "aarch64-windows-msvc-libcxx.cfg" "aarch64-windows-msvc" "$ABS_TOOLCHAINSPATH/windows-msvc-sysroot" "" "-D_DLL=1 -lmsvcrt -stdlib=libc++"
+create_cfg_file "i686-windows-msvc-libcxx.cfg" "i686-windows-msvc" "$ABS_TOOLCHAINSPATH/windows-msvc-sysroot" "" "-D_DLL=1 -lmsvcrt -stdlib=libc++"
 
 
 if [ ! -d "$LIBRARIES/fast_io" ]; then
