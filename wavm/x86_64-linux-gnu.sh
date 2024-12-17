b88212b785226dbb30203eb84babb7eb70918d25
trcrsired <uwgghhbcad@gmail.com>
2024-12-17 07:49:05 -0500

    loongarch64

--- wavm/x86_64-linux-gnu.sh
+++ wavm/x86_64-linux-gnu.sh
@@ -1,10 +1,2 @@
-if [ -z ${HOST+x} ]; then
-HOST=x86_64-linux-gnu
-fi
-
-if [ -z ${SYSTEMNAME+x} ]; then
-SYSTEMNAME=Linux
-fi
-
-HOST=$HOST SYSTEMNAME=$SYSTEMNAME ./wavm.sh "$@"
+HOST=x86_64-linux-gnu ./loongarch64-linux-gnu.sh "$@"
 exit $?
