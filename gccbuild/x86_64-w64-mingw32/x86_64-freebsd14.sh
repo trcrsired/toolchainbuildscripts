e925dd02dcd9945201e0e5ffeb07241e7ad71197
trcrsired <uwgghhbcad@gmail.com>
2024-12-17 11:44:14 -0500

    nothing

--- gccbuild/x86_64-w64-mingw32/x86_64-freebsd14.sh
+++ gccbuild/x86_64-w64-mingw32/x86_64-freebsd14.sh
@@ -260,7 +260,7 @@ if [[ ${hosttriple} == ${HOST} && ${MUSLLIBC} == "yes" ]]; then
 extra_binutils_configure_flags="--disable-plugins $extra_binutils_configure_flags"
 fi
 if [[ ${hosttriple} == ${HOST} ]]; then
-extra_binutils_configure_flags="--disable-gdb $extra_binutils_configure_flags"
+extra_binutils_configure_flags="$extra_binutils_configure_flags"
 fi
 STRIP=${hosttriple}-strip STRIP_FOR_TARGET=$HOSTSTRIP $TOOLCHAINS_BUILD/binutils-gdb/configure --disable-nls --disable-werror $ENABLEGOLD --prefix=$prefix --build=$BUILD --host=$hosttriple --target=$HOST $extra_binutils_configure_flags
 if [ $? -ne 0 ]; then
