7e900bb78fc0f1948b81b64b7ab31b8609e5cc0e
trcrsired <uwgghhbcad@gmail.com>
2025-01-05 06:07:14 -0500

    linux gnu

--- wine/linux-gnu.sh
+++ wine/linux-gnu.sh
@@ -14,7 +14,5 @@ fi
 if [ -z ${SYSROOT+x} ]; then
 SYSROOT=$TOOLCHAINSPATH_LLVM/$TRIPLE/$TRIPLE
 fi
-if [ -z ${MINIMUMBUILD+x} ]; then
-MINIMUMBUILD=yes
-fi
-CC="clang --target=$TRIPLE --sysroot=$SYSROOT -fuse-ld=lld -flto=thin -rtlib=compiler-rt --unwindlib=libunwind" CXX="clang++ --target=$TRIPLE --sysroot=$SYSROOT -fuse-ld=lld -flto=thin -rtlib=compiler-rt --unwindlib=libunwind -stdlib=libc++" MINIMUMBUILD=$MINIMUMBUILD CC_FOR_HOST="clang --target=$TRIPLE --sysroot=$SYSROOT -fuse-ld=lld -flto=thin -rtlib=compiler-rt --unwindlib=libunwind" CXX_FOR_HOST="clang++ --target=$TRIPLE --sysroot=$SYSROOT -fuse-ld=lld -flto=thin -rtlib=compiler-rt --unwindlib=libunwind -stdlib=libc++" ARCH=$ARCH HOST=$TRIPLE SYSROOT=$SYSROOT ./wine.sh "$@"
+
+CC="clang --target=$TRIPLE --sysroot=$SYSROOT -fuse-ld=lld -flto=thin -rtlib=compiler-rt --unwindlib=libunwind" CXX="clang++ --target=$TRIPLE --sysroot=$SYSROOT -fuse-ld=lld -flto=thin -rtlib=compiler-rt --unwindlib=libunwind -stdlib=libc++" CC_FOR_HOST="clang --target=$TRIPLE --sysroot=$SYSROOT -fuse-ld=lld -flto=thin -rtlib=compiler-rt --unwindlib=libunwind" CXX_FOR_HOST="clang++ --target=$TRIPLE --sysroot=$SYSROOT -fuse-ld=lld -flto=thin -rtlib=compiler-rt --unwindlib=libunwind -stdlib=libc++" ARCH=$ARCH HOST=$TRIPLE SYSROOT=$SYSROOT ./wine.sh "$@"
