db3f45b697b6a0bf34a05d8e9002f0b14d04d700
trcrsired <uwgghhbcad@gmail.com>
2025-02-01 17:48:02 -0500

    android

--- wine/aarch64-linux-android30.sh
+++ wine/aarch64-linux-android30.sh
@@ -1,4 +1,4 @@
 if [ -z ${TOOLCHAINSPATH+x} ]; then
 	TOOLCHAINSPATH=$HOME/toolchains
 fi
-enable_wineandroid_drv=no CC=clang CXX=clang++ HOST=aarch64-linux-android30 SYSROOT=$TOOLCHAINSPATH/llvm/aarch64-linux-android30/aarch64-linux-android30 ARCH=aarch64 CMAKEEXTRAFLAGS="-DCMAKE_CXX_FLAGS=\"-I$TOOLCHAINSPATH/llvm/aarch64-linux-android30/aarch64-linux-android30/include/c++/v1\"" ./wine.sh "$@"
+enable_wineandroid_drv=no CC=clang CXX=clang++ DISABLEALSA=yes HOST=aarch64-linux-android30 SYSROOT=$TOOLCHAINSPATH/llvm/aarch64-linux-android30/aarch64-linux-android30 ARCH=aarch64 CMAKEEXTRAFLAGS="-DCMAKE_CXX_FLAGS=\"-I$TOOLCHAINSPATH/llvm/aarch64-linux-android30/aarch64-linux-android30/include/c++/v1\"" ./wine.sh "$@"
