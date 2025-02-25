071c5284853c77cb379c6cf6006ff522e396a704
trcrsired <uwgghhbcad@gmail.com>
2025-02-25 16:54:02 -0500

    all

--- gccbuild/gnu/all.sh
+++ gccbuild/gnu/all.sh
@@ -1,7 +1,10 @@
-#if ! ./native.sh "$@"; then
-#exit 1
-#fi
-#if ! ./x86_64-w64-mingw32.sh "$@"; then
+if ! ./native.sh "$@"; then
+exit 1
+fi
+if ! ./x86_64-w64-mingw32.sh "$@"; then
+exit 1
+fi
+#if ! ./aarch64-w64-mingw32.sh "$@"; then
 #exit 1
 #fi
 if ! ./x86_64-linux-gnu.sh "$@"; then
@@ -16,9 +19,9 @@ fi
 if ! ./loongarch64-linux-gnu.sh "$@"; then
 exit 1
 fi
-#if ! ./loongarch64-linux-musl.sh "$@"; then
-#exit 1
-#fi
+if ! ./loongarch64-linux-musl.sh "$@"; then
+exit 1
+fi
 if ! ./x86_64-freebsd14.sh "$@"; then
 exit 1
 fi
