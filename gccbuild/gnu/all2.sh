2f56e149775251496cf54ba783876bc17742b0fb
trcrsired <uwgghhbcad@gmail.com>
2025-02-25 16:53:36 -0500

    all2?

--- gccbuild/gnu/all2.sh
+++ gccbuild/gnu/all2.sh
@@ -1,12 +1,12 @@
-#if ! ./native.sh "$@"; then
-#exit 1
-#fi
+if ! ./native.sh "$@"; then
+exit 1
+fi
 if ! ./x86_64-w64-mingw32.sh "$@"; then
 exit 1
 fi
-if ! ./aarch64-w64-mingw32.sh "$@"; then
-exit 1
-fi
+#if ! ./aarch64-w64-mingw32.sh "$@"; then
+#exit 1
+#fi
 if ! ./x86_64-linux-gnu.sh "$@"; then
 exit 1
 fi
