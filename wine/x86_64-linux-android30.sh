92294ec99783ad293b8e262075ea23b7958a15f3
trcrsired <uwgghhbcad@gmail.com>
2025-02-27 03:45:13 -0500

    minbuild

--- wine/x86_64-linux-android30.sh
+++ wine/x86_64-linux-android30.sh
@@ -1,4 +1,4 @@
 if [ -z ${TOOLCHAINSPATH+x} ]; then
 	TOOLCHAINSPATH=$HOME/toolchains
 fi
-enable_wineandroid_drv=no CC=clang CXX=clang++ DISABLEALSA=yes HOST=x86_64-linux-android30 SYSROOT=$TOOLCHAINSPATH/llvm/x86_64-linux-android30/x86_64-linux-android30 ARCH=x86_64 CMAKEEXTRAFLAGS="-DCMAKE_CXX_FLAGS=\"-I$TOOLCHAINSPATH/llvm/x86_64-linux-android30/x86_64-linux-android30/include/c++/v1\"" ./wine.sh "$@"
+enable_wineandroid_drv=no CC=clang CXX=clang++ MINIMUMBUILD=yes DISABLEALSA=yes HOST=x86_64-linux-android30 SYSROOT=$TOOLCHAINSPATH/llvm/x86_64-linux-android30/x86_64-linux-android30 ARCH=x86_64 CMAKEEXTRAFLAGS="-DCMAKE_CXX_FLAGS=\"-I$TOOLCHAINSPATH/llvm/x86_64-linux-android30/x86_64-linux-android30/include/c++/v1\"" ./wine.sh "$@"
