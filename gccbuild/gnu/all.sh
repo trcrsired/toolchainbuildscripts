006f13e323af8cd52690cf4be483f45df40e70f1
trcrsired <uwgghhbcad@gmail.com>
2025-02-27 11:10:17 -0500

    all.sh

--- gccbuild/gnu/all.sh
+++ gccbuild/gnu/all.sh
@@ -10,6 +10,15 @@ fi
 if ! ./aarch64-linux-gnu.sh "$@"; then
 exit 1
 fi
+if ! ./x86_64-w64-mingw32.sh "$@"; then
+exit 1
+fi
+if ! ./x86_64-linux-gnu.sh "$@"; then
+exit 1
+fi
+if ! ./aarch64-linux-gnu.sh "$@"; then
+exit 1
+fi
 if ! ./x86_64-elf.sh "$@"; then
 exit 1
 fi
