7c678131a4ee236be949a83a0ae575a62a46605b
trcrsired <uwgghhbcad@gmail.com>
2025-02-27 11:13:24 -0500

    all?

--- gccbuild/gnu/all.sh
+++ gccbuild/gnu/all.sh
@@ -22,12 +22,12 @@ fi
 if ! ./x86_64-elf.sh "$@"; then
 exit 1
 fi
-if ! ./loongarch64-linux-gnu.sh "$@"; then
-exit 1
-fi
-if ! ./loongarch64-linux-musl.sh "$@"; then
-exit 1
-fi
+#if ! ./loongarch64-linux-gnu.sh "$@"; then
+#exit 1
+#fi
+#if ! ./loongarch64-linux-musl.sh "$@"; then
+#exit 1
+#fi
 if ! ./x86_64-freebsd14.sh "$@"; then
 exit 1
 fi
