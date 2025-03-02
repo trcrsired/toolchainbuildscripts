7f860da5674890257870ac1fc578097537f62f73
trcrsired <uwgghhbcad@gmail.com>
2025-03-02 03:51:26 -0500

    no arch

--- gccbuild/gnu/x86_64-w64-mingw32.sh
+++ gccbuild/gnu/x86_64-w64-mingw32.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-if [ -z ${ARCH+x} ]; then
-	ARCH=x86_64
-fi
 
 if [ -z ${HOST+x} ]; then
-	HOST=${ARCH}-w64-mingw32
+	HOST=x86_64-w64-mingw32
 fi
 
 if [ -z ${CANADIANHOST+x} ]; then
 	CANADIANHOST=x86_64-w64-mingw32
 fi
 
-HOST=${HOST} ARCH=$ARCH CANADIANHOST=$CANADIANHOST ./riscv64-linux-gnu.sh
+HOST=${HOST} CANADIANHOST=${CANADIANHOST} ./riscv64-linux-gnu.sh
