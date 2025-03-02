6d70f17f475d0eeccf2d4774c6e20e60de562a1b
trcrsired <uwgghhbcad@gmail.com>
2025-03-01 19:50:02 -0500

    dos undef HAVE_FENV_H and _GLIBCXX_HAVE_FENV_H

--- gccbuild/gnu/i586-msdosdjgpp.sh
+++ gccbuild/gnu/i586-msdosdjgpp.sh
@@ -159,9 +159,10 @@ if [ ! -d "${CANADIANPREFIX}/lib/gcc" ]; then
     fi
 
     if [ -f "${currentpath}/hostbuild/$HOST/gcc/$TARGET/libstdc++/config.h" ]; then
-        sed -i 's/#define HAVE_FENV_H 1/#undef HAVE_FENV_H/' "${currentpath}/hostbuild/$HOST/gcc/$TARGET/libstdc++/config.h"
-        sed -i 's/#define _GLIBCXX_HAVE_FENV_H 1/#undef _GLIBCXX_HAVE_FENV_H/' "${currentpath}/hostbuild/$HOST/gcc/$TARGET/libstdc++/include/$TARGET/bits/c++config.h"
-        
+        # Replace #define HAVE_FENV_H 1 with /* #undef HAVE_FENV_H */
+        sed -i 's/#define HAVE_FENV_H 1/\/\* #undef HAVE_FENV_H \*\//' "${currentpath}/hostbuild/$HOST/gcc/$TARGET/libstdc++/config.h"
+        # Replace #define _GLIBCXX_HAVE_FENV_H 1 with /* #undef _GLIBCXX_HAVE_FENV_H */
+        sed -i 's/#define _GLIBCXX_HAVE_FENV_H 1/\/\* #undef _GLIBCXX_HAVE_FENV_H \*\//' "${currentpath}/hostbuild/$HOST/gcc/$TARGET/libstdc++/include/$TARGET/bits/c++config.h"
         make -j$(nproc)
         if [ $? -ne 0 ]; then
             echo "gcc (${HOST}/${TARGET}) build failed after modifying config.h"
