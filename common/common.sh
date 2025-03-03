b030cb63390d7715cc56dae22ab671240fff7cf3
trcrsired <uwgghhbcad@gmail.com>
2025-03-03 05:10:09 -0500

    common

--- common/common.sh
+++ common/common.sh
@@ -2,6 +2,7 @@ source ./safe-llvm-strip.sh
 source ./parse-triplet.sh
 source ./clone-dependencies.sh
 source ./build-glibc.sh
+source ./install-libc.sh
 ./dependencycheck.sh
 if [ $? -ne 0 ]; then
     exit 1
