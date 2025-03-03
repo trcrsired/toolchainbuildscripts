f2e31d87886dfd9683ccfd4ecd089298e6dd37a0
trcrsired <uwgghhbcad@gmail.com>
2025-03-03 03:23:26 -0500

    silence llvm strip

--- common/safe-llvm-strip.sh
+++ common/safe-llvm-strip.sh
@@ -17,6 +17,6 @@ function safe_llvm_strip {
     # Find all regular files in the specified directory and subdirectories
     find "$dir" -type f | while read -r file; do
         # Attempt to strip the file
-        llvm-strip --strip-unneeded "$file"
+        llvm-strip --strip-unneeded "$file" 2>/dev/null
     done
 }
