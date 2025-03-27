116250dca8b6995902e56f1393ce50e89b3982b4
trcrsired <uwgghhbcad@gmail.com>
2025-03-27 10:42:12 -0400

    typo error for common.sh

--- common/common.sh
+++ common/common.sh
@@ -5,7 +5,7 @@ source ./build-glibc.sh
 source ./install-libc.sh
 source ./check-location.sh
 source ./detect-platform.sh
-if [[ "xSKIP_DEPENDENCY_CHECK" != "xyes" ]]; then
+if [[ "x${SKIP_DEPENDENCY_CHECK}" != "xyes" ]]; then
 ./dependencycheck.sh
 if [ $? -ne 0 ]; then
     exit 1
