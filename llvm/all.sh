0a3786dad1f7c55efc6ba81e488147c54deef400
trcrsired <uwgghhbcad@gmail.com>
2025-04-26 19:36:32 +0800

    remove support

--- llvm/all.sh
+++ llvm/all.sh
@@ -39,7 +39,7 @@ main() {
 
 #    if [ "${ENABLE_RISCV_SUPPORT}" == "1" ]; then
         TRIPLETS2+=(
-            "riscv64-linux-android35"
+#            "riscv64-linux-android35"
             "riscv64-linux-gnu"
             "riscv64-linux-musl"
         )
