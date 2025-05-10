4a3b7a2eb7c2ca79dcc835eb3521c28908468c62
trcrsired <uwgghhbcad@gmail.com>
2025-05-10 12:21:59 +0800

    disable libxml2 test

--- llvm/build_common.sh
+++ llvm/build_common.sh
@@ -327,6 +327,9 @@ set(LIBXML2_WITH_ICONV Off)
 set(LIBXML2_WITH_PYTHON Off)
 set(BUILD_SHARED_LIBS Off)
 set(BUILD_STATIC_LIBS On)
+set(LIBXML2_WITH_TESTS OFF)
+set(LIBXML2_WITH_CATALOG OFF)
+set(LIBXML2_WITH_PROGRAMS Off)
 EOF
 
 cat << EOF > "$currentpath/cppwinrt.cmake"
@@ -475,11 +478,6 @@ set(CMAKE_RC_FLAGS "\${CMAKE_RC_FLAGS} -I\${CMAKE_SYSROOT}/include")
 EOF
 cat << EOF >> "$currentpath/zlib.cmake"
 set(ZLIB_BUILD_SHARED OFF)
-EOF
-cat << EOF >> "$currentpath/libxml2.cmake"
-set(LIBXML2_WITH_TESTS OFF)
-set(LIBXML2_WITH_CATALOG OFF)
-set(LIBXML2_WITH_PROGRAMS Off)
 EOF
 cat << EOF >> "$currentpath/llvm.cmake"
 unset(BUILD_SHARED_LIBS)
