5cb35f78bc708c91e53ea396eefc09453db1efea
trcrsired <uwgghhbcad@gmail.com>
2026-03-15 22:18:03 +0800

    gnu

--- gcc/x86_64-linux-gnu-build.sh
+++ gcc/x86_64-linux-gnu-build.sh
@@ -2,16 +2,17 @@
 set -e
 
 build_all() {
+    local build_main="x86_64-linux-gnu"
     local triplets=(
-        "x86_64-linux-gnu x86_64-linux-gnu x86_64-linux-gnu"
-        "x86_64-linux-gnu x86_64-w64-mingw32 x86_64-w64-mingw32"
-        "x86_64-linux-gnu aarch64-linux-gnu aarch64-linux-gnu"
-        "x86_64-linux-gnu x86_64-w64-mingw32 aarch64-linux-gnu"
-        "x86_64-linux-gnu loongarch64-linux-gnu loongarch64-linux-gnu"
-        "x86_64-linux-gnu x86_64-w64-mingw32 loongarch64-linux-gnu"
-        "x86_64-linux-gnu riscv64-linux-gnu riscv64-linux-gnu"
-        "x86_64-linux-gnu x86_64-w64-mingw32 riscv64-linux-gnu"
-        "x86_64-linux-gnu x86_64-w64-mingw32 x86_64-elf"
+        "$build_main x86_64-linux-gnu x86_64-linux-gnu"
+        "$build_main x86_64-w64-mingw32 x86_64-w64-mingw32"
+        "$build_main aarch64-linux-gnu aarch64-linux-gnu"
+        "$build_main x86_64-w64-mingw32 aarch64-linux-gnu"
+        "$build_main loongarch64-linux-gnu loongarch64-linux-gnu"
+        "$build_main x86_64-w64-mingw32 loongarch64-linux-gnu"
+        "$build_main riscv64-linux-gnu riscv64-linux-gnu"
+        "$build_main x86_64-w64-mingw32 riscv64-linux-gnu"
+        "$build_main x86_64-w64-mingw32 x86_64-elf"
     )
 
     for t in "${triplets[@]}"; do
