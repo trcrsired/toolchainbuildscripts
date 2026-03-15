b2e5555a829af7140e2abc642e4a1b547284f93d
trcrsired <uwgghhbcad@gmail.com>
2026-03-15 22:16:45 +0800

    x86_64-linux-gnu

---
+++ gcc/x86_64-linux-gnu-build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+
+build_all() {
+    local triplets=(
+        "x86_64-linux-gnu x86_64-linux-gnu x86_64-linux-gnu"
+        "x86_64-linux-gnu x86_64-w64-mingw32 x86_64-w64-mingw32"
+        "x86_64-linux-gnu aarch64-linux-gnu aarch64-linux-gnu"
+        "x86_64-linux-gnu x86_64-w64-mingw32 aarch64-linux-gnu"
+        "x86_64-linux-gnu loongarch64-linux-gnu loongarch64-linux-gnu"
+        "x86_64-linux-gnu x86_64-w64-mingw32 loongarch64-linux-gnu"
+        "x86_64-linux-gnu riscv64-linux-gnu riscv64-linux-gnu"
+        "x86_64-linux-gnu x86_64-w64-mingw32 riscv64-linux-gnu"
+        "x86_64-linux-gnu x86_64-w64-mingw32 x86_64-elf"
+    )
+
+    for t in "${triplets[@]}"; do
+        set -- $t
+        local build=$1
+        local host=$2
+        local target=$3
+
+        echo "=== Building for build=$build host=$host target=$target ==="
+
+        BUILD_TRIPLET=$build \
+        HOST_TRIPLET=$host \
+        TARGET_TRIPLET=$target \
+        ./build_common "$@"
+    done
+}
+
+build_all "$@"
+
