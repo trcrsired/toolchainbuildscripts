31d422e444f703697dea07810493b232effc9b38
trcrsired <uwgghhbcad@gmail.com>
2026-03-16 10:23:14 +0800

    testing

--- common/gcc_patches/gccx86canadianfix.py
+++ common/gcc_patches/gccx86canadianfix.py
@@ -2,13 +2,15 @@
 import sys
 import os
 
+# Check arguments
 if len(sys.argv) < 2:
-    print(f"Usage: {sys.argv[0]} <gcc source directory>")
+    prog = sys.argv[0] if sys.argv else "script.py"
+    print(f"Usage: {prog} <gcc source directory>")
     sys.exit(1)
 
 SRCDIR = sys.argv[1]
 
-# Read OLD block
+# Exact OLD block (must match byte-for-byte)
 OLD = r"""case $GCC,$host_os in
   yes,cygwin* | yes,mingw* | yes,pw32* | yes,cegcc*)
     library_names_spec='$libname.dll.a'
@@ -27,15 +29,14 @@ OLD = r"""case $GCC,$host_os in
        $RM \$dlpath'
     shlibpath_overrides_runpath=yes)"""
 
-# Read NEW block
+# Exact NEW block (must match C++ newvw exactly)
 NEW = r"""case $GCC,$host_os in
   yes,cygwin* | yes,mingw* | yes,pw32* | yes,cegcc*)
     library_names_spec='$libname.dll.a'
     # DLL is installed to $(libdir)/../bin by postinstall_cmds
-    # If user builds GCC with multilibs enabled,
-    # it should install into $(libdir) directly
-    # instead of $(libdir)/../bin, otherwise 32-bit DLLs
-    # may override 64-bit ones.
+    # If user builds GCC with mulitlibs enabled,
+    # it should just install on $(libdir)
+    # not on $(libdir)/../bin or 32 bits dlls would override 64 bit ones.
     if test x${multilib} = xyes; then
     postinstall_cmds='base_file=`basename \${file}`~
       dlpath=`$SHELL 2>&1 -c '\''. $dir/'\''\${base_file}'\''i; echo \$dlname'\''`~
@@ -61,25 +62,32 @@ NEW = r"""case $GCC,$host_os in
        $RM \$dlpath'
     shlibpath_overrides_runpath=yes)"""
 
+# Convert to bytes (exact UTF‑8 encoding)
 OLD_b = OLD.encode("utf-8")
 NEW_b = NEW.encode("utf-8")
 
+# Walk directory recursively
 for root, dirs, files in os.walk(SRCDIR):
     for name in files:
+        # Only process configure and libtool.m4
         if name not in ("configure", "libtool.m4"):
             continue
 
         path = os.path.join(root, name)
 
+        # Read file as bytes
         with open(path, "rb") as f:
             data = f.read()
 
+        # Skip if OLD block not found
         if OLD_b not in data:
             continue
 
         print("Patching", path)
 
+        # Replace all occurrences (byte-for-byte)
         newdata = data.replace(OLD_b, NEW_b)
 
+        # Write back
         with open(path, "wb") as f:
             f.write(newdata)
