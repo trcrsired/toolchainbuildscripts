374c9798afe5a55cfa4edae01dd4f6758a654086
trcrsired <uwgghhbcad@gmail.com>
2026-03-16 15:06:09 +0800

    build dos

--- gcc/all-build-x86_64-linux-gnu.sh
+++ gcc/all-build-x86_64-linux-gnu.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/env bash
 set -e
 
+restart_artifacts()
+{
+    local toolchainspath
+    local toolchains_path_gnu
+    if [ -z ${TOOLCHAINSPATH+x} ]; then
+        toolchainspath="$HOME/toolchains"
+    fi
+
+    if [ -z ${TOOLCHAINSPATH_GNU+x} ]; then
+        toolchains_path_gnu="$toolchainspath/gnu"
+    fi
+
+    echo "restarting"
+    rm -rf "$(realpath .)/.artifacts"
+    rm -rf "${toolchains_path_gnu}"
+    echo "restart done"
+}
+
+if [[ $1 == "restart" ]]; then
+restart_artifacts()
+fi
+
 build_all() {
     local build_main="x86_64-linux-gnu"
     local triplets=(
