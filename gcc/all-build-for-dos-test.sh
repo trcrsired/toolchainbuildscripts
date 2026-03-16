c1dda2ea3c4bdb502e8320953cf242c6d43d2758
trcrsired <uwgghhbcad@gmail.com>
2026-03-16 14:59:35 +0800

    dos

---
+++ gcc/all-build-for-dos-test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+build_all() {
+    local build_main="x86_64-linux-gnu"
+    local triplets=(
+        "$build_main x86_64-w64-mingw32 i586-msdosdjgpp"
+    )
+
+    for t in "${triplets[@]}"; do
+        set -- $t
+        local build=$1
+        local host=$2
+        local target=$3
+
+        echo "=== Building for build=$build host=$host target=$target ==="
+
+        BUILD_TRIPLET=$build \
+        HOST_TRIPLET=$host \
+        TARGET_TRIPLET=$target \
+        ./build_common.sh "$@"
+    done
+}
+
+build_all "$@"
+
