d5f379e425febc9ce08aa1f1f4d23f060ec0df24
trcrsired <uwgghhbcad@gmail.com>
2026-03-16 10:48:20 +0800

    canadian

--- common/gcc_patches/gccx86canadianfix.py
+++ common/gcc_patches/gccx86canadianfix.py
@@ -10,27 +10,27 @@ def main():
 
     srcdir = sys.argv[1]
 
-    # This must match the C++ vw block exactly (byte-for-byte)
-    OLD = r"""case $GCC,$host_os in
+    # EXACT COPY of C++ vw (byte-for-byte)
+    OLD = b"""case $GCC,$host_os in
   yes,cygwin* | yes,mingw* | yes,pw32* | yes,cegcc*)
     library_names_spec='$libname.dll.a'
     # DLL is installed to $(libdir)/../bin by postinstall_cmds
-    postinstall_cmds='base_file=`basename \${file}`~
-      dlpath=`$SHELL 2>&1 -c '\''. $dir/'\''\${base_file}'\''i; echo \$dlname'\''`~
-      dldir=$destdir/`dirname \$dlpath`~
-      test -d \$dldir || mkdir -p \$dldir~
-      $install_prog $dir/$dlname \$dldir/$dlname~
-      chmod a+x \$dldir/$dlname~
-      if test -n '\''$stripme'\'' && test -n '\''$striplib'\''; then
-        eval '\''$striplib \$dldir/$dlname'\'' || exit \$?;
+    postinstall_cmds='base_file=`basename \\${file}`~
+      dlpath=`$SHELL 2>&1 -c '\\''. $dir/'\\''\\${base_file}'\\''i; echo \\$dlname'\\''`~
+      dldir=$destdir/`dirname \\$dlpath`~
+      test -d \\$dldir || mkdir -p \\$dldir~
+      $install_prog $dir/$dlname \\$dldir/$dlname~
+      chmod a+x \\$dldir/$dlname~
+      if test -n '\\''$stripme'\\'' && test -n '\\''$striplib'\\''; then
+        eval '\\''$striplib \\$dldir/$dlname'\\'' || exit \\$?;
       fi'
-    postuninstall_cmds='dldll=`$SHELL 2>&1 -c '\''. $file; echo \$dlname'\''`~
-      dlpath=$dir/\$dldll~
-       $RM \$dlpath'
+    postuninstall_cmds='dldll=`$SHELL 2>&1 -c '\\''. $file; echo \\$dlname'\\''`~
+      dlpath=$dir/\\$dldll~
+       $RM \\$dlpath'
     shlibpath_overrides_runpath=yes)"""
 
-    # This must match the C++ newvw block exactly (byte-for-byte)
-    NEW = r"""case $GCC,$host_os in
+    # EXACT COPY of C++ newvw (byte-for-byte)
+    NEW = b"""case $GCC,$host_os in
   yes,cygwin* | yes,mingw* | yes,pw32* | yes,cegcc*)
     library_names_spec='$libname.dll.a'
     # DLL is installed to $(libdir)/../bin by postinstall_cmds
@@ -38,28 +38,28 @@ def main():
     # it should just install on $(libdir)
     # not on $(libdir)/../bin or 32 bits dlls would override 64 bit ones.
     if test x${multilib} = xyes; then
-    postinstall_cmds='base_file=`basename \${file}`~
-      dlpath=`$SHELL 2>&1 -c '\''. $dir/'\''\${base_file}'\''i; echo \$dlname'\''`~
-      dldir=$destdir/`dirname \$dlpath`~
+    postinstall_cmds='base_file=`basename \\${file}`~
+      dlpath=`$SHELL 2>&1 -c '\\''. $dir/'\\''\\${base_file}'\\''i; echo \\$dlname'\\''`~
+      dldir=$destdir/`dirname \\$dlpath`~
       $install_prog $dir/$dlname $destdir/$dlname~
       chmod a+x $destdir/$dlname~
-      if test -n '\''$stripme'\'' && test -n '\''$striplib'\''; then
-    eval '\''$striplib $destdir/$dlname'\'' || exit \$?;
+      if test -n '\\''$stripme'\\'' && test -n '\\''$striplib'\\''; then
+\teval '\\''$striplib $destdir/$dlname'\\'' || exit \\$?;
       fi'
     else
-    postinstall_cmds='base_file=`basename \${file}`~
-      dlpath=`$SHELL 2>&1 -c '\''. $dir/'\''\${base_file}'\''i; echo \$dlname'\''`~
-      dldir=$destdir/`dirname \$dlpath`~
-      test -d \$dldir || mkdir -p \$dldir~
-      $install_prog $dir/$dlname \$dldir/$dlname~
-      chmod a+x \$dldir/$dlname~
-      if test -n '\''$stripme'\'' && test -n '\''$striplib'\''; then
-    eval '\''$striplib \$dldir/$dlname'\'' || exit \$?;
+    postinstall_cmds='base_file=`basename \\${file}`~
+      dlpath=`$SHELL 2>&1 -c '\\''. $dir/'\\''\\${base_file}'\\''i; echo \\$dlname'\\''`~
+      dldir=$destdir/`dirname \\$dlpath`~
+      test -d \\$dldir || mkdir -p \\$dldir~
+      $install_prog $dir/$dlname \\$dldir/$dlname~
+      chmod a+x \\$dldir/$dlname~
+      if test -n '\\''$stripme'\\'' && test -n '\\''$striplib'\\''; then
+\teval '\\''$striplib \\$dldir/$dlname'\\'' || exit \\$?;
       fi'
     fi
-    postuninstall_cmds='dldll=`$SHELL 2>&1 -c '\''. $file; echo \$dlname'\''`~
-      dlpath=$dir/\$dldll~
-       $RM \$dlpath'
+    postuninstall_cmds='dldll=`$SHELL 2>&1 -c '\\''. $file; echo \\$dlname'\\''`~
+      dlpath=$dir/\\$dldll~
+       $RM \\$dlpath'
     shlibpath_overrides_runpath=yes)"""
 
     old_b = OLD.encode("utf-8")
