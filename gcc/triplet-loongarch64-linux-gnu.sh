99fc3ff33d4963d5c8d1b6d71074dc5eabbac97a
trcrsired <uwgghhbcad@gmail.com>
2026-03-17 16:04:16 +0800

    Remove most hosts

--- gcc/triplet-loongarch64-linux-gnu.sh
+++ gcc/triplet-loongarch64-linux-gnu.sh
@@ -21,7 +21,10 @@ GCCTRIPLETS=(
     "$GCC_BUILD_MAIN x86_64-w64-mingw32 x86_64-linux-musl"
     "$GCC_BUILD_MAIN x86_64-w64-mingw32 aarch64-linux-musl"
     "$GCC_BUILD_MAIN x86_64-w64-mingw32 i686-linux-musl"
+)
 
+GCC_EXTRA_HOSTS=(
+    # x86_64-linux-gnu hosts
     "$GCC_BUILD_MAIN x86_64-linux-gnu x86_64-w64-mingw32"
     "$GCC_BUILD_MAIN x86_64-linux-gnu aarch64-linux-gnu"
     "$GCC_BUILD_MAIN x86_64-linux-gnu loongarch64-linux-gnu"
@@ -33,6 +36,7 @@ GCCTRIPLETS=(
     "$GCC_BUILD_MAIN x86_64-linux-gnu aarch64-linux-musl"
     "$GCC_BUILD_MAIN x86_64-linux-gnu i686-linux-musl"
 
+    # aarch6464-linux-gnu hosts
     "$GCC_BUILD_MAIN aarch64-linux-gnu x86_64-w64-mingw32"
     "$GCC_BUILD_MAIN aarch64-linux-gnu x86_64-linux-gnu"
     "$GCC_BUILD_MAIN aarch64-linux-gnu loongarch64-linux-gnu"
@@ -44,4 +48,10 @@ GCCTRIPLETS=(
     "$GCC_BUILD_MAIN aarch64-linux-gnu aarch64-linux-musl"
     "$GCC_BUILD_MAIN aarch64-linux-gnu i686-linux-musl"
 )
-unset GCC_GCC_BUILD_MAIN
+
+if [[ "x$GCC_BUILD_ALL_HOSTS" == "xyes" ]]; then
+    GCCTRIPLETS+=("${EXTRA_HOSTS[@]}")
+fi
+
+unset GCC_BUILD_MAIN
+unset GCC_EXTRA_HOSTS
--- gcc/triplet-x86_64-linux-gnu.sh
+++ gcc/triplet-x86_64-linux-gnu.sh
@@ -20,8 +20,11 @@ GCCTRIPLETS=(
     "$GCC_BUILD_MAIN x86_64-w64-mingw32 i686-w64-mingw32"
     "$GCC_BUILD_MAIN x86_64-w64-mingw32 x86_64-linux-musl"
     "$GCC_BUILD_MAIN x86_64-w64-mingw32 aarch64-linux-musl"
-    "$GCC_BUILD_MAIN x86_64-w64-mingw32 i686-linux-musl"
+#    "$GCC_BUILD_MAIN x86_64-w64-mingw32 i686-linux-musl"
+)
 
+GCC_EXTRA_HOSTS=(
+    # aarch64-linux-gnu hosts
     "$GCC_BUILD_MAIN aarch64-linux-gnu x86_64-w64-mingw32"
     "$GCC_BUILD_MAIN aarch64-linux-gnu x86_64-linux-gnu"
     "$GCC_BUILD_MAIN aarch64-linux-gnu loongarch64-linux-gnu"
@@ -33,6 +36,7 @@ GCCTRIPLETS=(
     "$GCC_BUILD_MAIN aarch64-linux-gnu aarch64-linux-musl"
     "$GCC_BUILD_MAIN aarch64-linux-gnu i686-linux-musl"
 
+    # loongarch64-linux-gnu hosts
     "$GCC_BUILD_MAIN loongarch64-linux-gnu x86_64-w64-mingw32"
     "$GCC_BUILD_MAIN loongarch64-linux-gnu aarch64-linux-gnu"
     "$GCC_BUILD_MAIN loongarch64-linux-gnu x86_64-linux-gnu"
@@ -44,4 +48,10 @@ GCCTRIPLETS=(
     "$GCC_BUILD_MAIN loongarch64-linux-gnu aarch64-linux-musl"
     "$GCC_BUILD_MAIN loongarch64-linux-gnu i686-linux-musl"
 )
-unset GCC_GCC_BUILD_MAIN
+
+if [[ -n "$GCC_BUILD_ALL_HOSTS" ]]; then
+    GCCTRIPLETS+=("${EXTRA_HOSTS[@]}")
+fi
+
+unset GCC_BUILD_MAIN
+unset GCC_EXTRA_HOSTS
