91e1af44073ba3fab700564187abe3008d455fe1
trcrsired <oyzawqgcfc@gmail.com>
2026-07-23 03:15:35 +0800

    [llvm] remove freestandinglibcxx branch

--- llvm/freestanding_temp.sh
+++ llvm/freestanding_temp.sh
@@ -1,1 +1,1 @@
-LLVMPROJECTPATH=$HOME/toolchains_build/llvm_fixes/freestandingcxx/llvm-project FREESTANDING_LIBCXX=1 TRIPLET=x86_64-elf ./build_common.sh "$@"
+FREESTANDING_LIBCXX=1 TRIPLET=x86_64-elf ./build_common.sh "$@"
