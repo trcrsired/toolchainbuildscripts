82cf5f28fe3eacaeda6aa8f071251a6b2ab62612
trcrsired <uwgghhbcad@gmail.com>
2026-08-15 04:04:21 +0800

    extra flags

--- wine/aarch64-apple-darwin24.sh
+++ wine/aarch64-apple-darwin24.sh
@@ -17,12 +17,13 @@ if [ -z ${EXTRAPATH+x} ]; then
         EXTRAPATH=
     fi
 fi
+
 SYSROOT=$TOOLCHAINSPATH/llvm/aarch64-apple-darwin24/aarch64-apple-darwin24 \
 CC_FOR_HOST="clang --target=aarch64-apple-darwin24 --sysroot=$SYSROOT -mlinker-version=2.50"  \
 CXX_FOR_HOST="clang++ --target=aarch64-apple-darwin24 --sysroot=$SYSROOT -mlinker-version=2.50"  \
 CPP_FOR_HOST="clang-cpp --target=aarch64-apple-darwin24 --sysroot=$SYSROOT -mlinker-version=2.50"  \
 LDFLAGS="-fuse-ld=lld -mlinker-version=2.50"  \
-CONFIGUREEXTRAFLAGS="--without-x" \
+CONFIGUREEXTRAFLAGS="$CONFIGUREEXTRAFLAGS" \
 EXTRAPATH="$EXTRAPATH" \
 enable_wineandroid_drv=no CC=clang CXX=clang++ HOST=aarch64-apple-darwin24 ARCH=aarch64 NO_CREATE_LIBPTHREAD=yes BUILDDEPENDENCIES="no"  \
 ./wine.sh "$@"
