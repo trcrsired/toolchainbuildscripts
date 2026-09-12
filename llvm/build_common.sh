820312c36a625f5ce2981be36c80dab1a4b2d497
trcrsired <oyzawqgcfc@gmail.com>
2026-09-12 16:23:27 +0800

    remove the runetable slop

--- llvm/build_common.sh
+++ llvm/build_common.sh
@@ -653,13 +653,6 @@ set(CMAKE_SYSTEM_NAME Generic)
 EOF
 fi
 
-if [[ "$OS" == "wasi"* ]]; then
-cat << EOF >> "$currentpath/runtimes.cmake"
-set(CMAKE_C_FLAGS_INIT "\${CMAKE_C_FLAGS_INIT} -D_LIBCPP_PROVIDES_DEFAULT_RUNE_TABLE")
-set(CMAKE_CXX_FLAGS_INIT "\${CMAKE_CXX_FLAGS_INIT} -D_LIBCPP_PROVIDES_DEFAULT_RUNE_TABLE")
-EOF
-fi
-
 cat << EOF >> "$currentpath/runtimes.cmake"
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
