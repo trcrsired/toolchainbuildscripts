5475476efe38405befe01a7259d1407f418f9b67
trcrsired <oyzawqgcfc@gmail.com>
2026-09-14 02:01:07 +0800

    aarch64 windows gnu

--- wavm/aarch64-windows-gnu.sh
+++ wavm/aarch64-windows-gnu.sh
@@ -6,5 +6,5 @@ if [ -z ${SYSTEMNAME+x} ]; then
 SYSTEMNAME=Windows
 fi
 
-HOST=$HOST SYSTEMNAME=$SYSTEMNAME ./x86_64-windows-gnu.sh "$@"
+HOST=$HOST SYSTEMNAME=$SYSTEMNAME EXTRAFLAGS="-DCMAKE_C_LINKER_DEPFILE_SUPPORTED=Off -DCMAKE_CXX_LINKER_DEPFILE_SUPPORTED=Off -DCMAKE_ASM_LINKER_DEPFILE_SUPPORTED=Off -Wl,--section-alignment=0x10000 -Wl,/driver" EXTRACFLAGS="-rtlib=compiler-rt --unwindlib=libunwind" EXTRACXXFLAGS="-rtlib=compiler-rt --unwindlib=libunwind -stdlib=libc++ -lunwind -lc++abi" ./wavm.sh "$@"
 exit $?
