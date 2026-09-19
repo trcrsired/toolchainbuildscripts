d927452521d16e357e9b5532e8c6f29f35700257
trcrsired <oyzawqgcfc@gmail.com>
2026-09-20 04:29:42 +0800

    wasm

--- llvm/wasm-sysroots.sh
+++ llvm/wasm-sysroots.sh
@@ -24,10 +24,10 @@ ARTIFACTS_ROOT="$(realpath "$SCRIPT_DIR")/.artifacts"
 
 # Define all WASI sysroot variants
 WASI_VARIANTS=(
-	"wasm-sysroot"
-	"wasm-sysroot-mtg"
-	"wasm-sysroot-noeh"
 	"wasm-sysroot-noeh-mtg"
+	"wasm-sysroot-noeh"
+	"wasm-sysroot-mtg"
+	"wasm-sysroot"
 )
 
 # Define all WASI targets
